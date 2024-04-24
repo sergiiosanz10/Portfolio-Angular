a5b05a7b9e48b050c2308080c680279b4f8c56de
Sergio <sergiosanz2004@gmail.com>
2024-04-24 10:12:21 +0200

    contacto funcionando

--- src/app/portfolio/pages/contact-page/contact-page.component.ts
+++ src/app/portfolio/pages/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './contact-page.component.scss'
 })
 export class ContactPageComponent {
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_bbu0o6g', 'template_bf3ksfr', e.target as HTMLFormElement, {
+        publicKey: 'rsQJ4VOdU8W3p53zn',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 
 }
