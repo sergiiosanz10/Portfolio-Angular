c7b1943afb8fc4fb6a54575ffe1c7b17a9b1b00c
Sergio <sergiosanz2004@gmail.com>
2024-04-24 11:16:11 +0200

    About me

--- src/app/portfolio/pages/home-page/home-page.component.ts
+++ src/app/portfolio/pages/home-page/home-page.component.ts
@@ -7,16 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
-  text: string = `Hello! My name is Sergio Sanz, and I am a passionate Front-end developer. I specialize in creating interactive, efficient, and sophisticated web applications using the latest technologies and frameworks.
-
-  Over the years, I have honed my skills in HTML, CSS, JavaScript, and TypeScript. I have a solid experience with frameworks such as Angular and React, and I'm also familiar with Bootstrap for user interface design. In addition, I have experience with CSS preprocessors like Sass.
-
-  On the server side, I have experience with Node.js and PHP, and I'm comfortable using jQuery for DOM manipulations and effects. I'm also familiar with package handling with npm.
-
-  I have worked on a variety of projects, from simple static websites to complex web applications. I enjoy the challenge of solving problems and creating solutions that not only meet the requirements but also provide a great user experience.
-
-  In addition to my technical skills, I am a strong communicator and a team player. I believe that collaboration and clear communication are key to the success of any project.
-
-  I'm always looking for new opportunities to apply my skills and contribute to exciting projects. If you're interested in working with me, please don't hesitate to get in touch!`;
-
+  text: string = `Hello! My name is Sergio Sanz, and I am a passionate Front-end developer. I specialize in creating interactive, efficient, and sophisticated
+                  web applications using the latest technologies and frameworks.
+                  Over the years, I have honed my skills in HTML, CSS, JavaScript, and TypeScript. I have a solid experience with frameworks such as Angular...`
 }
