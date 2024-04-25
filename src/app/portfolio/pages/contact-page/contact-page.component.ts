10f5e7ae6256bb699804bb661b6f7c7f2896a9d2
Sergio Sanz <sergiosanz2004@gmail.com>
2024-04-25 18:23:12 +0200

    cookie para evitar spam y hacer esperar 30 segundos

--- src/app/portfolio/pages/contact-page/contact-page.component.ts
+++ src/app/portfolio/pages/contact-page/contact-page.component.ts
@@ -11,23 +11,51 @@ export class ContactPageComponent {
 
   constructor(private customCursor: CustomCursorComponent) { }
 
-  public sendEmail(e: Event) {
-    e.preventDefault();
+public ngOnInit() {
+  // Guarda una cookie con la fecha y hora actuales cuando el usuario entra a la página
+  document.cookie = `lastEmailSentAt=${Date.now()}; path=/`;
+}
 
-    emailjs
-      .sendForm('service_bbu0o6g', 'template_bf3ksfr', e.target as HTMLFormElement, {
-        publicKey: 'rsQJ4VOdU8W3p53zn',
-      })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-          location.reload();
-        },
-        (error) => {
-          console.log('FAILED...', (error as EmailJSResponseStatus).text);
-        },
-      );
+public sendEmail(e: Event) {
+  e.preventDefault();
+
+  const now = Date.now();
+  const lastEmailSentAt = this.getCookie('lastEmailSentAt');
+  const timeSinceLastEmail = now - (lastEmailSentAt || 0);
+
+  // Limita los correos electrónicos a uno cada 30 segundos
+  if (timeSinceLastEmail < 30000) {
+    console.log('Por favor, espera antes de enviar otro correo electrónico.');
+    return;
   }
+
+  // Actualiza la cookie con la fecha y hora actuales
+  document.cookie = `lastEmailSentAt=${now}; path=/`;
+
+  emailjs
+    .sendForm('service_bbu0o6g', 'template_bf3ksfr', e.target as HTMLFormElement, {
+      publicKey: 'rsQJ4VOdU8W3p53zn',
+    })
+    .then(
+      () => {
+        console.log('SUCCESS!');
+        location.reload();
+      },
+      (error) => {
+        console.log('FAILED...', (error as EmailJSResponseStatus).text);
+      },
+    );
+}
+
+private getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    const cookieValue = parts.pop()?.split(';').shift();
+    return Number(cookieValue);
+  }
+  return null;
+}
 
 
   onMouseEnter() {
