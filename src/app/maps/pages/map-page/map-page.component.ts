4f6542fa709e6a88ef760ee364d1bd4e859c1b43
Sergio Sanz García <sergiosanz2004@gmail.com>
2024-05-08 13:00:39 +0200

    terminado

--- src/app/maps/pages/map-page/map-page.component.ts
+++ src/app/maps/pages/map-page/map-page.component.ts
@@ -1,9 +1,9 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Map, Marker, Popup } from 'mapbox-gl';
+import { LngLat, Map, Marker, Popup } from 'mapbox-gl';
 import { MapsService } from '../../services/maps.service';
 import { GeoPoint2D, Province, Result } from '../../interfaces/maps-interface';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { switchMap, tap } from 'rxjs';
+import { pipe, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-map-page',
@@ -18,7 +18,8 @@ export class MapPageComponent {
   public provinces: Province | undefined;
   public municipios: Province | undefined;
   public marker: Marker | undefined;
-  public selectedMunicipio: Result | undefined
+  public selectedMunicipio: Result | undefined;
+  public weather: string | undefined
 
   public myForm: FormGroup = this.fb.group({
     comunidad: ['', Validators.required],
@@ -76,6 +77,8 @@ export class MapPageComponent {
       )
       .subscribe(data => {
         this.municipios = data;
+        console.log(this.municipios);
+
       });
 
     this.myForm.get('municipio')!.valueChanges
@@ -86,9 +89,8 @@ export class MapPageComponent {
 
   flyTo(municipio: string) {
     this.selectedMunicipio = this.municipios?.results.find(m => m.mun_name === municipio);
-    console.log(this.selectedMunicipio);
 
-    if(this.selectedMunicipio==undefined) return;
+    if (this.selectedMunicipio == undefined) return;
     if (this.map && this.selectedMunicipio.geo_point_2d) {
 
       this.map.flyTo({
@@ -101,26 +103,71 @@ export class MapPageComponent {
       })
       this.marker.setLngLat([this.selectedMunicipio.geo_point_2d.lon, this.selectedMunicipio.geo_point_2d.lat]).addTo(this.map);
 
-      this.weather()
+      this.municipioWeather()
+      this.mapsService.getweather(this.selectedMunicipio.geo_point_2d.lon, this.selectedMunicipio.geo_point_2d.lat)
     }
   }
 
-  weather(){
+  municipioWeather() {
     //Div y boton para redirigir a weather
-    this.marker?.getElement().addEventListener("click",() => {
+    this.marker?.getElement().addEventListener("click", () => {
       console.log("HILI");
     })
     let popup = new Popup()
     let div = document.createElement("div");
     let bnt = document.createElement("a");
     div.classList.add('div_btn');
-    bnt.innerText="Tiempo";
+    bnt.innerText = "Tiempo";
     bnt.classList.add("btn_weather");
 
-    bnt.setAttribute("href",`/portfolio/projects/weather/${this.selectedMunicipio?.mun_name}`);
+    bnt.setAttribute("href", `/portfolio/projects/weather/${this.selectedMunicipio?.mun_name}`);
     div.appendChild(bnt);
 
     popup.setDOMContent(div);
     this.marker?.setPopup(popup);
+  }
+
+  createMarker() {
+    if (!this.map) return;
+
+    const color = '#xxxxxx'.replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+    const lngLat = this.map.getCenter();
+
+    this.addMarker(lngLat, color);
+  }
+
+  addMarker(lngLat: LngLat, color: string) {
+
+    if (!this.map) return;
+
+    this.marker = new Marker({
+      color: color,
+      draggable: true
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map);
+
+    this.marker.on('dragend', () => console.log(this.marker));
+    this.weatherMarker(lngLat)
+
+  }
+
+  weatherMarker(lngLat: LngLat) {
+
+    this.mapsService.getweather(lngLat.lat, lngLat.lng)
+      .subscribe(({ name }) => {
+        this.weather = name;
+
+        this.mapsService.getWeatherMunicipio(name)
+          .subscribe(data => {
+            this.myForm.get('comunidad')?.setValue(data.results[0].acom_name),
+            this.myForm.get('provincia')!.setValue(data.results[0].prov_name)
+            this.myForm.get('municipio')!.setValue(data.results[0].mun_name),
+            this.selectedMunicipio = data.results[0]
+            this.municipioWeather()
+          }
+          )
+      });
+
   }
 }
