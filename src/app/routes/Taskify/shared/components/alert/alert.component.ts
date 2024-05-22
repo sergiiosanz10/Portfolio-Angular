6a9f47265511c6d21c37c18f0e1483482c3f3b6a
Sergio Sanz García <sergiosanz2004@gmail.com>
2024-05-22 09:14:59 +0200

    alerts personalizados

--- src/app/routes/Taskify/shared/components/alert/alert.component.ts
+++ src/app/routes/Taskify/shared/components/alert/alert.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'shared-alert',
   templateUrl: './alert.component.html',
 })
 export class AlertComponent {
+
+  @Input()
+  public message: string = '';
+
+  @Input()
+  public alertType: string = 'alert-danger'
 
 }
